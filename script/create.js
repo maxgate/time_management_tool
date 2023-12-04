a158481bbccc87147ca098f5d295837170a2b040
max vincent <kelechivincy@gmail.com>
2023-12-04 03:04:01 -0800

    updated

--- script/create.js
+++ script/create.js
@@ -1,17 +1,21 @@
+const displaySchedule = document.querySelector(".display");
+
 const createSchedule = (item) => {
-  const displaySchedule = document.querySelector(".display");
   const html = `
-  <div class="container-xxl justify-content-around align-content-center pt-5 col-4 small">
-          <div
-            class="col-md-2 col-lg-4  rounded-top shadow text-center text-light pt-1 pb-1"
+      <div class="container-xxl justify-content-around align-content-center pt-5 col-4  small">
+        <div
+            class=" col-md-4  col-lg-4  rounded-top shadow text-center text-light pt-1 pb-1"
             style="background-color: #6664c2"
           >          
             ${item.time_manager_title}
-          </div>
-          <div class=" line col-md-2 col-lg-4  bg-light rounded-bottom shadow text-center small pt-2 pb-2" >
-            ${item.time_manager_message}
-          </div>
         </div>
+        <div class=" line col-md-4 col-lg-4  bg-light rounded-bottom shadow text-center small pt-2 pb-2" >
+            ${item.time_manager_message}
+        </div>
+        <div>
+          <button class="clear btn btn-danger mx-sm-5 mx-s mx-md-2 my-2  mx-lg-5 small  fw-bold" onClick="clearBtn(${item.id})">clear</button>
+        </div>
+      </div>
 `;
 
   displaySchedule.insertAdjacentHTML("beforeEnd", html);
@@ -24,13 +28,40 @@ function createTodosStoragse() {
   }
 }
 
+const todos = localStorage.getItem("todos");
+
 function createNewSchedule() {
   createTodosStoragse();
 
   const todos = localStorage.getItem("todos");
   JSON.parse(todos).forEach((item) => {
     createSchedule(item);
+    // localStorage.clear();
+    // // todos = "";
   });
 }
 
 createNewSchedule();
+
+////////////////////////////
+// Clear function
+const clear = document.querySelectorAll(".clear");
+
+const clearBtn = (id) => {
+  console.log(id);
+  let getItem = JSON.parse(todos);
+  deleteBtn = getItem.filter((item) => item.id !== id);
+  localStorage.setItem("todos", JSON.stringify(deleteBtn));
+};
+
+// clear.forEach((e, id) => {
+//   e.addEventListener("click", clearBtn(id));
+// });
+
+// clear.forEach((id) => {
+// const anotherTodo = JSON.parse(todos);
+// anotherTodo.pop((item) => {
+//   item.id != id;
+// });
+// console.log(anotherTodo);
+// });
--- script/script.js
+++ script/script.js
@@ -1,11 +1,11 @@
 const schedule = document.getElementById("mov");
+// const clear = document.querySelectorAll(".clear");
 
 const createTodosStoragse = () => {
   const item = localStorage.getItem("todos");
   if (!item) {
     localStorage.setItem("todos", JSON.stringify([]));
   }
-  
 };
 
 schedule.addEventListener("click", () => {
@@ -15,11 +15,20 @@ schedule.addEventListener("click", () => {
   const todoArray = JSON.parse(todos);
   const title = document.getElementById("exampleFormControlInput1").value;
   const note = document.querySelector(".note").value;
+  const id = todoArray.length + 1;
   const newTodoArray = [
     ...todoArray,
-    { time_manager_message: note, time_manager_title: title },
+    {
+      id,
+      time_manager_message: note,
+      time_manager_title: title,
+    },
   ];
   localStorage.setItem("todos", JSON.stringify(newTodoArray));
 
   window.location.href = "../index.html";
 });
+
+// clear.addEventListener("click", () => {
+//   console.log("cleared");
+// });
